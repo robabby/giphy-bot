40a80d3d5ce2c7093ad94510f0fde5c54fcbbf02
robabby <robabby23@gmail.com>
2017-05-25 22:44:52 -0500

    Ping Pong

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+/*
+  A ping pong bot, whenever you send "ping", it replies "pong".
+*/
+
+// Import the discord.js module
 const Discord = require('discord.js');
+
+// Create an instance of a Discord client
 const client = new Discord.Client();
-const giphy = require( 'giphy' )( 'dc6zaTOxFJmzC' );
+
+// The token of your bot - https://discordapp.com/developers/applications/me
+const token = 'MzE3NDk3OTA0MzEzMTM5MjIx.DAktHA.WhKZQ-n0lqf2e9RKIowgyjnt1GI';
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
+
+// Log our bot in
+client.login(token);
